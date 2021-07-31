8b2e72b94fda769fe766bb5b208744b6c22e00f3
DonutLaser <vidmantas.luneckas@gmail.com>
2021-07-31 15:40:26 +0300

    Show multiple images and switch them with arrow keys

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"path"
+	"strings"
+
 	"github.com/veandco/go-sdl2/img"
 	"github.com/veandco/go-sdl2/sdl"
 )
 
-func load_image(path string, renderer *sdl.Renderer) *sdl.Texture {
+func findImagesInDir(dir string, supportedTypes []string) (result []string) {
+	files, err := ioutil.ReadDir(dir)
+	checkError(err)
+
+	for _, file := range files {
+		extension := strings.ToLower(path.Ext(file.Name()))
+		if containsString(supportedTypes, extension) {
+			result = append(result, fmt.Sprintf("%s/%s", dir, file.Name()))
+		}
+	}
+
+	return
+}
+
+func loadImage(path string, renderer *sdl.Renderer) *sdl.Texture {
 	image, err := img.Load(path)
 	checkError(err)
 
@@ -15,6 +34,12 @@ func load_image(path string, renderer *sdl.Renderer) *sdl.Texture {
 	image.Free()
 
 	return texture
+}
+
+func render(renderer *sdl.Renderer, image *sdl.Texture) {
+	renderer.Clear()
+	renderer.Copy(image, nil, nil)
+	renderer.Present()
 }
 
 func main() {
@@ -30,22 +55,51 @@ func main() {
 	checkError(err)
 	defer renderer.Destroy()
 
-	image := load_image("D:/Wallpapers/Canyon.jpg", renderer)
-	defer image.Destroy()
+	supportedImages := []string{".png", ".jpg", ".bmp"} // Must have dots
+	currentImage := 0
+	images := findImagesInDir("D:/Wallpapers", supportedImages)
 
 	renderer.SetDrawColor(0, 0, 0, 255)
+
+	image := loadImage(images[currentImage], renderer)
+	defer image.Destroy()
 
 	running := true
 	for running {
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-			switch event.(type) {
+			switch t := event.(type) {
 			case *sdl.QuitEvent:
 				running = false
+			case *sdl.KeyboardEvent:
+				keyCode := t.Keysym.Sym
+
+				switch keyCode {
+				case sdl.K_RIGHT:
+					if t.Repeat > 0 || t.State == sdl.RELEASED {
+						break
+					}
+
+					nextImage := clamp(currentImage+1, 0, len(images))
+					if nextImage != currentImage {
+						currentImage = nextImage
+						image.Destroy()
+						image = loadImage(images[currentImage], renderer)
+					}
+				case sdl.K_LEFT:
+					if t.Repeat > 0 || t.State == sdl.RELEASED {
+						break
+					}
+
+					nextImage := clamp(currentImage-1, 0, len(images))
+					if nextImage != currentImage {
+						currentImage = nextImage
+						image.Destroy()
+						image = loadImage(images[currentImage], renderer)
+					}
+				}
 			}
 		}
 
-		renderer.Clear()
-		renderer.Copy(image, nil, nil)
-		renderer.Present()
+		render(renderer, image)
 	}
 }
--- utility.go
+++ utility.go
@@ -1,9 +1,31 @@
 package main
-	
+
 import "log"
 
 func checkError(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func containsString(arr []string, value string) bool {
+	for _, item := range arr {
+		if item == value {
+			return true
+		}
+	}
+
+	return false
+}
+
+func clamp(value int, min int, max int) int {
+	if value < min {
+		return min
+	}
+
+	if value > max {
+		return max
+	}
+
+	return value
 }
