5749ce4d25c75895072af4c72a17664338936299
DonutLaser <vidmantas.luneckas@gmail.com>
2021-07-31 14:26:34 +0300

    Showing image in the window

--- main.go
+++ main.go
@@ -1,15 +1,39 @@
 package main
 
-import "github.com/veandco/go-sdl2/sdl"
+import (
+	"github.com/veandco/go-sdl2/img"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+func load_image(path string, renderer *sdl.Renderer) *sdl.Texture {
+	image, err := img.Load(path)
+	checkError(err)
+
+	texture, err := renderer.CreateTextureFromSurface(image)
+	checkError(err)
+
+	image.Free()
+
+	return texture
+}
 
 func main() {
 	err := sdl.Init(sdl.INIT_EVERYTHING)
 	checkError(err)
 	defer sdl.Quit()
 
-	window, err := sdl.CreateWindow("image-viewer", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 800, 600, sdl.WINDOW_SHOWN)
+	window, err := sdl.CreateWindow("Image Viewer", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 800, 600, sdl.WINDOW_RESIZABLE)
 	checkError(err)
 	defer window.Destroy()
+
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	checkError(err)
+	defer renderer.Destroy()
+
+	image := load_image("D:/Wallpapers/Canyon.jpg", renderer)
+	defer image.Destroy()
+
+	renderer.SetDrawColor(0, 0, 0, 255)
 
 	running := true
 	for running {
@@ -19,5 +43,9 @@ func main() {
 				running = false
 			}
 		}
+
+		renderer.Clear()
+		renderer.Copy(image, nil, nil)
+		renderer.Present()
 	}
 }
